d2723c4bd9fa0973dc70398dd176ec390276336f
Nicholas Suski <n33kos@gmail.com>
2016-05-14 11:34:56 -0600

    added update to apt-get

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+sudo apt-get -y update
 sudo apt-get -y install python-numpy python-pip python-dev
 sudo pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.8.0-cp27-none-linux_x86_64.whl
